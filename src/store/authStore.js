7c738c82b3b553360be2fa8b01c3848ffcecfe0f
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2025-01-02 21:34:57 -0800

    Added login function

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 import { auth } from "$lib/firebase/firebase.client";
-import { createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, updateEmail, updatePassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, updateEmail, updatePassword } from "firebase/auth";
 
 export const authStore = writable({
     isLoading: true,
@@ -22,6 +22,9 @@ export const authHandlers = {
     },
     updatePassword: async (password) => {
         await updatePassword(auth, password)
+    },
+    login: async (email, password) => {
+        await signInWithEmailAndPassword(auth, email, password)
     }
 }
 

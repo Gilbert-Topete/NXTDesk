c0c72aa2e1832b5b070b9df43f87924385ec4197
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2025-01-03 18:21:30 -0800

    Added Loading to Dashboard

--- src/routes/privatedashboard/+page.svelte
+++ src/routes/privatedashboard/+page.svelte
@@ -1,1 +1,14 @@
-test
+<script>
+	import { authStore } from "../../store/authStore";
+
+    let email;
+    authStore.subscribe((curr) => {
+        email = curr?.currentUser?.email;
+    })
+</script>
+
+{#if $authStore.currentUser}
+    <h1>Welcome back, {email}</h1>
+{:else}
+    <div>Loading...</div>
+{/if}

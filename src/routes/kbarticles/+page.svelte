6472af9a6c749c190bf11e22b62ddcd096550176
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2025-01-12 21:46:15 -0800

    Created "kbarticles", "profile", and "supporttickets" pages

---
+++ src/routes/kbarticles/+page.svelte
@@ -0,0 +1,31 @@
+<!-- KB Articles page -->
+<script>
+	import AuthReset from "../../components/AuthReset.svelte";
+	import Sidebar from "../../components/Sidebar.svelte";
+    import { authHandlers, authStore } from "../../store/authStore";
+
+    let email;
+    authStore.subscribe((curr) => {
+        email = curr?.currentUser?.email;
+    })
+</script>
+
+{#if $authStore.currentUser}
+    <h1>Welcome back, {email}</h1>
+    <Sidebar />
+    <div class="container">
+        <h1>Knowledgebase article page under construction. Come back later!</h1>
+    </div>
+{:else}
+    <div>Loading...</div>
+{/if}
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+    }
+</style>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,20 @@
+<!-- Private Dashboard page -->
+<script>
+	import AuthReset from "../../components/AuthReset.svelte";
+	import Sidebar from "../../components/Sidebar.svelte";
+    import { authHandlers, authStore } from "../../store/authStore";
+
+    let email;
+    authStore.subscribe((curr) => {
+        email = curr?.currentUser?.email;
+    })
+</script>
+
+{#if $authStore.currentUser}
+    <h1>Welcome back, {email}</h1>
+    <Sidebar />
+    <AuthReset />
+    <button on:click={authHandlers.logout}>Logout</button>
+{:else}
+    <div>Loading...</div>
+{/if}

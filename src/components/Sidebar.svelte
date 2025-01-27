5383f06852cc95dd12eb5250580cd1253f683afd
Gilbert Topete <gtopete2004@csu.fullerton.edu>
2025-01-26 21:44:51 -0800

    Moved home button

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -3,18 +3,12 @@
 </script>
 
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-  <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+  <a href="/home" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
     <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    <span class="fs-4">Sidebar</span>
+    <span class="fs-4">Home</span>
   </a>
   <hr>
   <ul class="nav nav-pills flex-column mb-auto">
-    <li class="nav-item">
-      <a href="/home" class="nav-link active" aria-current="page">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
-        Home
-      </a>
-    </li>
     <li>
       <a href="/supporttickets" class="nav-link text-white">
         <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
@@ -22,7 +16,7 @@
       </a>
     </li>
     <li>
-      <a href="kbarticles" class="nav-link text-white">
+      <a href="/kbarticles" class="nav-link text-white">
         <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
         Knowledgebase Articles
       </a>
